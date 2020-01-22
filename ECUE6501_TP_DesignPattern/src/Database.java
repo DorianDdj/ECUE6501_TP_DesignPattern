2185514f24cc758e00570d07ffb86e6b04e1d605
VANLERBERGHE Michael <v19009228@A-F004229.salsa.univ-amu.fr>
2020-01-22 09:04:40 +0100

    classe Badge et BadgeFactory

--- ECUE6501_TP_DesignPattern/src/Database.java
+++ ECUE6501_TP_DesignPattern/src/Database.java
@@ -3,21 +3,24 @@ import java.util.Map;
 
 /* Simule la base de données non implémentée */
 public class Database {
-	/*données <numéro, type d'utilisateur>
-	 ex: <"19009228", "etudiant"> */
-	Map<String, String> data = new HashMap<>();
+	//données <numéro, type d'utilisateur>
+	//ex: <"19009228", "etudiant">
+	static Map<String, String> data = new HashMap<>();
 	
 	//Simule une requête insert
 	//prend en paramètre le numéro d'utilisateur et son type
 	static void insert(String id, String type) {
-		
+		data.put(id, type);
 	}
 	
 	//Simule une requête select
 	//prend en paramètre le numéro d'utilisateur
-	//renvoie le type d'utilisateur
+	//renvoie le type d'utilisateur s'il existe, sinon null
 	static String select(String id) {
-		return "";
+		if(data.containsKey(id))
+			return data.get(id);
+		else
+			return null;
 	}
 	
 }
